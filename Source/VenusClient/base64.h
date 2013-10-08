358186bea2de14224e425dd9468598d9928d7050
jackiesteed <jackiecpp@gmail.com>
2013-10-08 16:36:38 +0800

    Revert "First Version and Last Version"
    This reverts commit eb292398a681e23b1580c808ec4e6ac1d0bf7ecc.

--- Source/VenusClient/base64.h
+++
@@ -1,8 +0,0 @@
-#ifndef	__BASE64__H__
-#define __BASE64__H__
-
-
-int base64_encode( const unsigned char* bytes_to_encode, int in_len, char* crypt);
-int base64_decode(char* encoded_string, unsigned char* plain);
-
-#endif /*_BASE64__H_*/
